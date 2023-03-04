f8a4c865ef09ccdb2f72ffaf24a6a3de7e0464b7
Adamor Henner <adacandido9@gmail.com>
2023-03-03 22:25:56 -0300

    Tratando erros do Login

--- src/telas/Login/index.js
+++ src/telas/Login/index.js
@@ -4,14 +4,30 @@ import Botao from '../../componentes/Botao';
 import { EntradaTexto } from '../../componentes/EntradaTexto';
 import { logar } from '../../servicos/requisicoesFirebase';
 import estilos from './estilos';
+import { Alerta } from '../../componentes/Alerta';
 
 export default function Login({ navigation }) {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  const [statusError, setStatusError] = useState('');
+  const [mensagemError, setMensagemError] = useState('');
 
   async function realizarLogin(){
-    const resultado = await logar(email,senha);
-    console.log(resultado);
+    if(email == ''){
+      setMensagemError('O E-mail é obrigatorio!');
+      setStatusError('email');
+    } else if (senha == ''){
+      setMensagemError('A Senha é obrigatoria!');
+      setStatusError('senha');
+    } else {
+      const resultado = await logar(email,senha);
+      if ( resultado == 'error') {
+        setStatusError('firebase')
+        setMensagemError('E-mail ou senha não conferem')
+      } else {
+        navigation.navigate('Principal')
+      }
+    }
   }
 
   return (
@@ -20,14 +36,24 @@ export default function Login({ navigation }) {
         label="E-mail"
         value={email}
         onChangeText={texto => setEmail(texto)}
+        error={statusError == 'email'}
+        messageError={mensagemError}
       />
       <EntradaTexto
         label="Senha"
         value={senha}
         onChangeText={texto => setSenha(texto)}
         secureTextEntry
+        error={statusError == 'senha'}
+        messageError={mensagemError}
       />
       
+      <Alerta
+        mensagem={mensagemError}
+        error={statusError == 'firebase'}
+        setError={setStatusError}
+      />
+
       <Botao onPress={() => realizarLogin()}>LOGAR</Botao>
       <Botao 
         onPress={() => { navigation.navigate('Cadastro') }}
