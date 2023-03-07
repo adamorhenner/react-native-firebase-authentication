5b44a38a646f40e132f35af1d811b85dc5baf03a
Adamor Henner <adacandido9@gmail.com>
2023-03-07 00:12:16 -0300

    Refatorando as validações do login

---
+++ src/telas/Login/entradas.js
@@ -0,0 +1,16 @@
+export const entradas = [
+    {
+      id: "1",
+      name: "email",
+      label: "E-mail",
+      mensagemError: "Digite um e-mail válido",
+      secureTextEntry: false,
+    },
+    {
+      id: "2",
+      name: "senha",
+      label: "Senha",
+      mensagemError: "Digite uma senha válida",
+      secureTextEntry: true,
+    },
+  ];
--- src/telas/Login/index.js
+++ src/telas/Login/index.js
@@ -1,52 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { View, Image } from 'react-native';
-import Botao from '../../componentes/Botao';
-import { EntradaTexto } from '../../componentes/EntradaTexto';
-import { logar } from '../../servicos/requisicoesFirebase';
-import estilos from './estilos';
-import { Alerta } from '../../componentes/Alerta';
-import { auth } from '../../config/firebase';
-import animacaoCarregando from '../../../assets/animacaoCarregando.gif';
-import { alteraDados } from '../../utils/comum';
+import React, { useEffect, useState } from "react";
+import { View, Image } from "react-native";
+import Botao from "../../componentes/Botao";
+import { EntradaTexto } from "../../componentes/EntradaTexto";
+import { logar } from "../../servicos/requisicoesFirebase";
+import estilos from "./estilos";
+import { Alerta } from "../../componentes/Alerta";
+import { auth } from "../../config/firebase";
+import animacaoCarregando from "../../../assets/animacaoCarregando.gif";
+import { alteraDados } from "../../utils/comum";
+import { entradas } from "./entradas";
 
 
 export default function Login({ navigation }) {
   const [dados, setDados] = useState({
-    email:'',
-    senha:''
-  })
+    email: "",
+    senha: "",c
+  });
 
-  const [statusError, setStatusError] = useState('');
-  const [mensagemError, setMensagemError] = useState('');
+  const [statusError, setStatusError] = useState("");
+  const [mensagemError, setMensagemError] = useState("");
   const [carregando, setCarregando] = useState(true);
 
+
+
   useEffect(() => {
-    const estadoUsuario = auth.onAuthStateChanged(
-    usuario => {
-      if (usuario){
-        navigation.replace('Principal')
+    const estadoUsuario = auth.onAuthStateChanged((usuario) => {
+      if (usuario) {
+        navigation.replace("Principal");
       }
-      setCarregando(false)
-    })
+      setCarregando(false);
+    });
     return () => estadoUsuario();
-  }, [])
+  }, []);
 
-
-  async function realizarLogin(){
-    if(dados.email == ''){
-      setMensagemError('O E-mail é obrigatorio!');
-      setStatusError('email');
-    } else if (dados.senha == ''){
-      setMensagemError('A Senha é obrigatoria!');
-      setStatusError('senha');
+  async function realizarLogin() {
+    if (dados.email == "") {
+      setMensagemError("O E-mail é obrigatorio!");
+      setStatusError("email");
+    } else if (dados.senha == "") {
+      setMensagemError("A Senha é obrigatoria!");
+      setStatusError("senha");
     } else {
-      const resultado = await logar(dados.email,
-        dados.senha);
-      if ( resultado == 'error') {
-        setStatusError('firebase')
-        setMensagemError('E-mail ou senha não conferem')
+      const resultado = await logar(dados.email, dados.senha);
+      if (resultado == "error") {
+        setStatusError("firebase");
+        setMensagemError("E-mail ou senha não conferem");
       } else {
-        navigation.replace('Principal')
+        navigation.replace("Principal");
       }
     }
   }
@@ -54,42 +54,35 @@ export default function Login({ navigation }) {
   if (carregando) {
     return (
       <View style={estilos.containerAnimacao}>
-        <Image source={animacaoCarregando}
-          style={estilos.imagem}
-        />
+        <Image source={animacaoCarregando} style={estilos.imagem} />
       </View>
-    )
+    );
   }
 
   return (
     <View style={estilos.container}>
-      <EntradaTexto 
-        label="E-mail"
-        value={dados.email}
-        onChangeText={valor => alteraDados('email',
-        valor, dados, setDados)}
-        error={statusError == 'email'}
-        messageError={mensagemError}
-      />
-      <EntradaTexto
-        label="Senha"
-        value={dados.senha}
-        onChangeText={valor => alteraDados('senha',
-        valor, dados, setDados)}
-        secureTextEntry
-        error={statusError == 'senha'}
-        messageError={mensagemError}
-      />
-      
+      {entradas.map((entrada) => {
+        return (
+          <EntradaTexto
+            key={entrada.id}
+            {...entrada}
+            onChangeText={valor => alteraDados
+              (entrada.name, valor, dados, setDados)}
+          />
+        );
+      })}
+
       <Alerta
         mensagem={mensagemError}
-        error={statusError == 'firebase'}
+        error={statusError == "firebase"}
         setError={setStatusError}
       />
 
       <Botao onPress={() => realizarLogin()}>LOGAR</Botao>
-      <Botao 
-        onPress={() => { navigation.navigate('Cadastro') }}
+      <Botao
+        onPress={() => {
+          navigation.navigate("Cadastro");
+        }}
       >
         CADASTRAR USUÁRIO
       </Botao>
